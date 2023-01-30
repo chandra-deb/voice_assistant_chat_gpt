d763ae40ff79d2164a861a3e8277ea3a6312dae5
chandra-deb <mail.chandradeb@gmail.com>
2023-01-31 02:35:30 +0600

    constants lives here

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,6 +1,10 @@
 // Colors
-
 import 'dart:ui';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 const backgroundColor = Color(0xff343541);
 const botBackgroundColor = Color(0xff444654);
+
+// ApiKey
+final apiKey = dotenv.env['apiKey'];
