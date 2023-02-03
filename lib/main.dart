1df2f586c42a8cfd8ac46c6945981f79b2a73f44
chandra-deb <mail.chandradeb@gmail.com>
2023-02-04 00:31:32 +0600

    create tts to change notifier provider

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       title: 'Friendly',
       home: MultiProvider(
         providers: [
-          Provider<TextToSpeechProvider>(
+          ChangeNotifierProvider<TextToSpeechProvider>(
             create: (context) => TextToSpeechProvider(),
           ),
           Provider<ConversationProvider>(
--- lib/providers/text_to_speech_provider.dart
+++ lib/providers/text_to_speech_provider.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 
-class TextToSpeechProvider {
+class TextToSpeechProvider extends ChangeNotifier {
   bool isSpeaking = false;
   late final FlutterTts _tts;
 
@@ -13,18 +14,21 @@ class TextToSpeechProvider {
     required String text,
     void Function()? setOnSpeakingCompletion,
   }) async {
-    isSpeaking = true;
     await _tts.speak(text);
+    isSpeaking = true;
+    notifyListeners();
     _tts.setCompletionHandler(() {
       if (setOnSpeakingCompletion != null) {
         setOnSpeakingCompletion();
       }
       isSpeaking = false;
+      notifyListeners();
     });
   }
 
   Future<void> stopSpeaking() async {
     isSpeaking = false;
+    notifyListeners();
     await _tts.stop();
   }
 }
