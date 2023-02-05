9255c24621e10ae5f40bd746bfbb8f1765601322
chandra-deb <mail.chandradeb@gmail.com>
2023-02-05 16:33:43 +0600

    add dummy message

--- lib/providers/messages_list_provider.dart
+++ lib/providers/messages_list_provider.dart
@@ -3,7 +3,104 @@ import 'package:flutter/foundation.dart';
 import '../models/chat_message_model.dart';
 
 class MessagesListProvider extends ChangeNotifier {
-  final List<ChatMessage> _messages = [];
+  final List<ChatMessage> _messages = [
+    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
+    ChatMessage(
+        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
+    ChatMessage(
+        text: 'I am fine. What about you?',
+        chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
+    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
+    ChatMessage(
+        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
+    ChatMessage(
+        text: 'I am fine. What about you?',
+        chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
+    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
+    ChatMessage(
+        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
+    ChatMessage(
+        text: 'I am fine. What about you?',
+        chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
+    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
+    ChatMessage(
+        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
+    ChatMessage(
+        text: 'I am fine. What about you?',
+        chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
+    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
+    ChatMessage(
+        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
+    ChatMessage(
+        text: 'I am fine. What about you?',
+        chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
+    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
+    ChatMessage(
+        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
+    ChatMessage(
+        text: 'I am fine. What about you?',
+        chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
+    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
+    ChatMessage(
+        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
+    ChatMessage(
+        text: 'I am fine. What about you?',
+        chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
+    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
+    ChatMessage(
+        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
+    ChatMessage(
+        text: 'I am fine. What about you?',
+        chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
+    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
+    ChatMessage(
+        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
+    ChatMessage(
+        text: 'I am fine. What about you?',
+        chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
+    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
+    ChatMessage(
+        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
+    ChatMessage(
+        text: 'I am fine. What about you?',
+        chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
+    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
+    ChatMessage(
+        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
+    ChatMessage(
+        text: 'I am fine. What about you?',
+        chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
+    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
+    ChatMessage(
+        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
+    ChatMessage(
+        text: 'I am fine. What about you?',
+        chatMessageType: ChatMessageType.user),
+    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
+    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
+  ];
 
   List<ChatMessage> get messages => _messages;
 
