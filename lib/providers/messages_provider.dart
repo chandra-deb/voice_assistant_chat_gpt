756637aaf1b784cf622519fd9fe655a3caa2e9f6
chandra-deb <mail.chandradeb@gmail.com>
2023-02-06 15:54:35 +0600

    renamed msgList provider and add st msg func

--- lib/providers/messages_provider.dart
+++ lib/providers/messages_provider.dart
@@ -2,7 +2,14 @@ import 'package:flutter/foundation.dart';
 
 import '../models/chat_message_model.dart';
 
-class MessagesListProvider extends ChangeNotifier {
+class MessagesProvider extends ChangeNotifier {
+  ChatMessage? _selectedMessage;
+  ChatMessage? get selectedMessage => _selectedMessage;
+  int _previousMessagesLength = 0;
+  int _messagesLength = 0;
+  int get previousMessageLength => _previousMessagesLength;
+  int get messagesLength => _messagesLength;
+
   final List<ChatMessage> _messages = [
     ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
     ChatMessage(
@@ -104,8 +111,30 @@ class MessagesListProvider extends ChangeNotifier {
 
   List<ChatMessage> get messages => _messages;
 
+  void setSelectedMessage(ChatMessage chatMessage) {
+    _selectedMessage = chatMessage;
+
+    notifyListeners();
+  }
+
+  void clearSelectedMessage() {
+    _selectedMessage = null;
+    notifyListeners();
+  }
+
   void addMessage(ChatMessage chatMessage) {
+    _previousMessagesLength = _messages.length;
     _messages.add(chatMessage);
+    _messagesLength = messages.length;
+    notifyListeners();
+  }
+
+  void deleteMessage(ChatMessage chatMessage) {
+    _previousMessagesLength = messages.length;
+    _messages.remove(chatMessage);
+    _messagesLength = messages.length;
+
+    clearSelectedMessage();
     notifyListeners();
   }
 }
