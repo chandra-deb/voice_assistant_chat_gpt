1432c627757272da1330b7a68e541c144a647bb9
chandra-deb <mail.chandradeb@gmail.com>
2023-02-06 16:00:46 +0600

    now every chatMessage has an id

--- lib/models/chat_message_model.dart
+++ lib/models/chat_message_model.dart
@@ -1,10 +1,13 @@
+import 'package:uuid/uuid.dart';
+
 enum ChatMessageType { user, bot }
 
 class ChatMessage {
+  final String id;
   final String text;
   final ChatMessageType chatMessageType;
   ChatMessage({
     required this.text,
     required this.chatMessageType,
-  });
+  }) : id = const Uuid().v4();
 }
