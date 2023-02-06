69e5021f4e5d11e1840d4e47d65c3c106886d16b
chandra-deb <mail.chandradeb@gmail.com>
2023-02-06 16:03:30 +0600

    home page now use bottom actions widget

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,10 +3,10 @@ import 'package:provider/provider.dart';
 
 import 'models/chat_message_model.dart';
 import 'providers/conversation_provider.dart';
-import 'providers/messages_list_provider.dart';
+import 'providers/messages_provider.dart';
 import 'providers/text_to_speech_provider.dart';
+import 'widgets/mini/bottom_actions_widget.dart';
 import 'widgets/mini/chat_message_list_widget.dart';
-import 'widgets/mini/input_bar_widget.dart';
 import 'widgets/mini/sp_indicator.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,12 +17,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late final MessagesListProvider messagesListProvider;
+  late final MessagesProvider messagesListProvider;
 
   @override
   void initState() {
     super.initState();
-    messagesListProvider = context.read<MessagesListProvider>();
+    messagesListProvider = context.read<MessagesProvider>();
   }
 
   Future<void> messageAdder(String text) async {
@@ -71,8 +71,12 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-      onTapDown: (details) => FocusManager.instance.primaryFocus?.unfocus(),
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      onTapDown: (details) {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
       child: Scaffold(
         appBar: AppBar(
           centerTitle: true,
@@ -81,46 +85,20 @@ class _HomePageState extends State<HomePage> {
             padding: EdgeInsets.all(8.0),
             child: SPIndicatorWidget(),
           ),
-          // backgroundColor: botBackgroundColor,
           elevation: 0,
           backgroundColor: Colors.white,
         ),
-        // backgroundColor: backgroundColor,
         body: SafeArea(
             child: Container(
           // padding: const EdgeInsets.all(15),
           child: Column(
             children: [
               Expanded(
-                // child: Container(
-                // color: Colors.red,
                 child: ChatMessageListViewWidget(),
-                // ),
               ),
-              Consumer<TextToSpeechProvider>(
-                builder: (context, ttsProvider, child) {
-                  return AnimatedSwitcher(
-                    duration: const Duration(milliseconds: 400),
-                    transitionBuilder:
-                        (Widget child, Animation<double> animation) {
-                      return ScaleTransition(scale: animation, child: child);
-                    },
-                    child: ttsProvider.isSpeaking
-                        ? GestureDetector(
-                            onTap: () {
-                              ttsProvider.stopSpeaking();
-                            },
-                            child: Container(
-                              color: Colors.pink,
-                              height: 50,
-                              width: 70,
-                              child: const Icon(Icons.close),
-                            ),
-                          )
-                        : InputBarWidget(addMessage: messageAdder),
-                  );
-                },
-              ),
+              BottomActionsWidget(
+                messageAdder: messageAdder,
+              )
             ],
           ),
         )),
---
+++ lib/widgets/mini/bottom_actions_widget.dart
@@ -0,0 +1,60 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:voice_chat_gpt/widgets/mini/message_long_tap_actions_widget.dart';
+
+import '../../models/chat_message_model.dart';
+import '../../providers/messages_provider.dart';
+import '../../providers/text_to_speech_provider.dart';
+import 'input_bar_widget.dart';
+
+class BottomActionsWidget extends StatelessWidget {
+  final Future<void> Function(String text) messageAdder;
+  const BottomActionsWidget({
+    Key? key,
+    required this.messageAdder,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final isSpeaking =
+        context.select<TextToSpeechProvider, bool>((tts) => tts.isSpeaking);
+    final selectedMessage = context
+        .select<MessagesProvider, ChatMessage?>((msg) => msg.selectedMessage);
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 400),
+      transitionBuilder: (Widget child, Animation<double> animation) {
+        return FadeTransition(
+          opacity: animation,
+          child: child,
+        );
+
+        // return ScaleTransition(scale: animation, child: child);
+      },
+      child: isSpeaking
+          ? GestureDetector(
+              onTap: () {
+                context.read<TextToSpeechProvider>().stopSpeaking();
+                context.read<MessagesProvider>().clearSelectedMessage();
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.pink,
+                  border: Border.all(width: 0.1, color: Colors.grey),
+                  borderRadius: const BorderRadius.all(Radius.circular(20)),
+                ),
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                height: 50,
+                width: 70,
+                child: const Icon(
+                  Icons.close,
+                  color: Colors.white,
+                ),
+              ),
+            )
+          : selectedMessage != null
+              ? const MessageLongTapActionsWidget()
+              : InputBarWidget(addMessage: messageAdder),
+    );
+  }
+}
