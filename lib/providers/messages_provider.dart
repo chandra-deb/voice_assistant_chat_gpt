41423259447722eda583adf78176967ddc48df43
chandra-deb <mail.chandradeb@gmail.com>
2023-02-08 01:31:43 +0600

    clearing the clutters

--- lib/providers/messages_provider.dart
+++ lib/providers/messages_provider.dart
@@ -1,113 +1,13 @@
 import 'package:flutter/foundation.dart';
 
 import '../models/chat_message_model.dart';
+import 'dmlist.dart';
 
 class MessagesProvider extends ChangeNotifier {
   ChatMessage? _selectedMessage;
   ChatMessage? get selectedMessage => _selectedMessage;
-  int _previousMessagesLength = 0;
-  int _messagesLength = 0;
-  int get previousMessageLength => _previousMessagesLength;
-  int get messagesLength => _messagesLength;
 
-  final List<ChatMessage> _messages = [
-    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
-    ChatMessage(
-        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
-    ChatMessage(
-        text: 'I am fine. What about you?',
-        chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
-    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
-    ChatMessage(
-        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
-    ChatMessage(
-        text: 'I am fine. What about you?',
-        chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
-    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
-    ChatMessage(
-        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
-    ChatMessage(
-        text: 'I am fine. What about you?',
-        chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
-    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
-    ChatMessage(
-        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
-    ChatMessage(
-        text: 'I am fine. What about you?',
-        chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
-    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
-    ChatMessage(
-        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
-    ChatMessage(
-        text: 'I am fine. What about you?',
-        chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
-    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
-    ChatMessage(
-        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
-    ChatMessage(
-        text: 'I am fine. What about you?',
-        chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
-    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
-    ChatMessage(
-        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
-    ChatMessage(
-        text: 'I am fine. What about you?',
-        chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
-    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
-    ChatMessage(
-        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
-    ChatMessage(
-        text: 'I am fine. What about you?',
-        chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
-    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
-    ChatMessage(
-        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
-    ChatMessage(
-        text: 'I am fine. What about you?',
-        chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
-    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
-    ChatMessage(
-        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
-    ChatMessage(
-        text: 'I am fine. What about you?',
-        chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
-    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
-    ChatMessage(
-        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
-    ChatMessage(
-        text: 'I am fine. What about you?',
-        chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
-    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'Hi', chatMessageType: ChatMessageType.user),
-    ChatMessage(
-        text: 'Hey! How are you?', chatMessageType: ChatMessageType.bot),
-    ChatMessage(
-        text: 'I am fine. What about you?',
-        chatMessageType: ChatMessageType.user),
-    ChatMessage(text: 'I am fine, too.', chatMessageType: ChatMessageType.bot),
-    ChatMessage(text: 'Last Message', chatMessageType: ChatMessageType.user),
-  ];
+  final List<ChatMessage> _messages = [...dummyMessages];
 
   List<ChatMessage> get messages => _messages;
 
@@ -123,16 +23,12 @@ class MessagesProvider extends ChangeNotifier {
   }
 
   void addMessage(ChatMessage chatMessage) {
-    _previousMessagesLength = _messages.length;
     _messages.add(chatMessage);
-    _messagesLength = messages.length;
     notifyListeners();
   }
 
   void deleteMessage(ChatMessage chatMessage) {
-    _previousMessagesLength = messages.length;
     _messages.remove(chatMessage);
-    _messagesLength = messages.length;
 
     clearSelectedMessage();
     notifyListeners();
