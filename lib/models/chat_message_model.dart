3b604ab02d253e910f0115f709bbf23a8d0d3399
chandra-deb <mail.chandradeb@gmail.com>
2023-02-09 00:07:21 +0600

    chatMessageType and ChatMessage Hive model

--- lib/models/chat_message_model.dart
+++ lib/models/chat_message_model.dart
@@ -1,11 +1,25 @@
+import 'package:hive/hive.dart';
 import 'package:uuid/uuid.dart';
 
-enum ChatMessageType { user, bot }
+part 'chat_message_model.g.dart';
 
+@HiveType(typeId: 0)
+enum ChatMessageType {
+  @HiveField(0)
+  user,
+  @HiveField(1)
+  bot,
+}
+
+@HiveType(typeId: 1)
 class ChatMessage {
+  @HiveField(0)
   final String id;
+  @HiveField(1)
   final String text;
+  @HiveField(2)
   final ChatMessageType chatMessageType;
+
   ChatMessage({
     required this.text,
     required this.chatMessageType,
