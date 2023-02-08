f1f5470fa40a57d025fad3d711558d1fd2f35125
chandra-deb <mail.chandradeb@gmail.com>
2023-02-09 00:07:58 +0600

    add messagesBox ref

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -2,9 +2,14 @@
 import 'dart:ui';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive/hive.dart';
+import 'package:voice_chat_gpt/models/chat_message_model.dart';
 
 const backgroundColor = Color(0xff343541);
 const botBackgroundColor = Color(0xff444654);
 
 // ApiKey
 final apiKey = dotenv.env['apiKey'];
+
+// HiveDB
+final messagesBox = Hive.box<ChatMessage>('messagesBox');
